9728e52052b48622940ea5ffdf033649a8246bb7
iPython X <innat1994@gmail.com>
2018-08-21 04:50:53 +0600

    Update Ostu's Method.py

--- Segmentation/Threshold/Ostu-s Method/Ostu's Method.py
+++ Segmentation/Threshold/Ostu-s Method/Ostu's Method.py
@@ -1,3 +1,12 @@
+
+"""
+Image Data Analysis Using Numpy & OpenCV
+author: Mohammed Innat
+email:  innat1994@gmail.com
+website: https://iphton.github.io/iphton.github.io/
+Please feel free to use and modify this, but keep the above information. Thanks!
+"""
+
 import numpy as np
 import imageio
 import matplotlib.pyplot as plt
