3fc625e31062924b9b4eea8b21d87b5d3aa9030c
iPython X <innat1994@gmail.com>
2018-08-21 00:43:06 +0600

    Add files via upload

---
+++ Segmentation/Threshold/Ostu-s Method/Ostu's Method.py
@@ -0,0 +1,40 @@
+import numpy as np
+import imageio
+import matplotlib.pyplot as plt
+
+pic = imageio.imread('<image location>')
+plt.imshow(pic);
+
+def otsu_threshold(im):
+
+    # Compute histogram and probabilities of each intensity level
+    pixel_counts = [np.sum(im == i) for i in range(256)]
+
+    # Initialization
+    s_max = (0,0)
+
+    for threshold in range(256):
+
+        # update
+        w_0 = sum(pixel_counts[:threshold])
+        w_1 = sum(pixel_counts[threshold:])
+
+        mu_0 = sum([i * pixel_counts[i] for i in range(0,threshold)]) / w_0 if w_0 > 0 else 0
+        mu_1 = sum([i * pixel_counts[i] for i in range(threshold, 256)]) / w_1 if w_1 > 0 else 0
+
+        # calculate - inter class variance
+        s = w_0 * w_1 * (mu_0 - mu_1) ** 2
+
+        if s > s_max[1]:
+            s_max = (threshold, s)
+
+
+    return s_max[0]
+
+
+def threshold(pic, threshold):
+    return ((pic > threshold) * 255).astype('uint8')
+
+gray = lambda rgb : np.dot(rgb[... , :3] , [0.21 , 0.72, 0.07])
+plt.imshow(threshold(gray(pic), otsu_threshold(pic)), cmap='Greys')
+plt.axis('off')
